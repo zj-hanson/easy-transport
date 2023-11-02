169301943b22c53b55edeb15f1dfc14c67b7ac6c
C0160 <dtianyu@hanbell.cn>
2023-11-02 10:55:29 +0800

    Update EasyTransportApplication.java

--- src/main/java/com/lightshell/transport/EasyTransportApplication.java
+++ src/main/java/com/lightshell/transport/EasyTransportApplication.java
@@ -2,9 +2,10 @@ package com.lightshell.transport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableJpaRepositories
 public class EasyTransportApplication {
 
